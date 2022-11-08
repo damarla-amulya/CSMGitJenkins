6806323a751f281b39f614f19bcefb9ce6eb21eb
sruthisetty1@gmail.com <sruthisetty1@gmail.com>
2022-11-08 10:53:14 +0530

    Sample.java created at local repo

---
+++ Sample.java
@@ -0,0 +1,7 @@
+public class Sample
+{
+           public static void main(String a[])
+           {
+                  System.out.println("This file created in local git  and pushed to github and run through jenkins");
+           }
+}
